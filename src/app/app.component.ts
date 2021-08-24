ff09c2370aca21a1a990c6c9a45fa06b0df9a906
guih58 <matheus.guih58@gmail.com>
2021-08-24 17:18:55 -0300

    Criado formGrup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,46 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Todo } from 'src/models/to.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  public todos: any[] = [];
-  public title: String = "Minha tarefas"
+  public todos: Todo[] = [];
+  public title: String = "Minha tarefas";
+  public form: FormGroup;
   
+
+  constructor(private fb: FormBuilder) {
+    this.form= this.fb.group({
+      titulo: ['' , Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required
+      ])],
+      
+      
+    })
+    this.todos.push( new Todo(1 , "Matheus Guilherme " ,false))
+    this.todos.push( new Todo(2 , "Matheus Guilherme " ,false))
+    this.todos.push( new Todo(3 , "Matheus Guilherme " ,true))
   
-  constructor() {
-    this.todos.push("Passar com matheus")
-    this.todos.push("Passar com matheus")
-    this.todos.push("Passar com matheus")
-
-
-    
   }
+  remove(todo: Todo){
+  const index = this.todos.indexOf(todo)
+  if(index != -1){
+    this.todos.splice(index, 1)
+  }
+
+  }
+  markAsDone(todo: Todo){
+    todo.done = true;
+  }
+  markAsUnDone(todo: Todo){
+    todo.done = false;
+  }
+ 
 }
